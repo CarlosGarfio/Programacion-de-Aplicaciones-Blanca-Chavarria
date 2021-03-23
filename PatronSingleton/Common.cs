0a0b599d3102337ef16305cca2860b0f501a864b
Carlos Daniel Garfio Murillo <gmcarlosd@outlook.com>
2021-03-22 19:11:56 -0700

    Patrón Singleton

---
+++ PatronSingleton/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicios.PatronSingleton
+{
+    public sealed class Common
+    {
+        private static readonly Common _instance = new Common();
+
+        private Common()
+        {
+        }
+
+        public static Common Instance => _instance;
+
+        public void SaySomething(string message) => Console.WriteLine(message);
+    }
+}
---
+++ PatronSingleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicios.PatronSingleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Probando mi singleton";
+            Common.Instance.SaySomething(msg);
+        }
+    }
+}
